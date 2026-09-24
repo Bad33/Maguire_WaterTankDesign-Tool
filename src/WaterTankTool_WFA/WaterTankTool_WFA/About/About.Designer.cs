20a4047026f31b80f3cb21c7475af1d525442d3e
Nikhil Chaudhary <44422494+Bad33@users.noreply.github.com>
2026-09-24 12:02:54 -0500

    Update About.Designer.cs

--- src/WaterTankTool_WFA/WaterTankTool_WFA/About/About.Designer.cs
+++ src/WaterTankTool_WFA/WaterTankTool_WFA/About/About.Designer.cs
@@ -29,41 +29,13 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
-            pictureBox1 = new PictureBox();
-            pictureBox2 = new PictureBox();
             label1 = new Label();
             label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
             label5 = new Label();
             label6 = new Label();
-            label7 = new Label();
-            label8 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(398, 32);
-            pictureBox1.Margin = new Padding(2, 2, 2, 2);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(92, 79);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(43, 32);
-            pictureBox2.Margin = new Padding(2, 2, 2, 2);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(95, 79);
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox2.TabIndex = 1;
-            pictureBox2.TabStop = false;
             // 
             // label1
             // 
@@ -87,27 +59,7 @@
             label2.TabIndex = 4;
             label2.Text = "Water Tank Analysis Tool";
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(161, 141);
-            label3.Margin = new Padding(2, 0, 2, 0);
-            label3.Name = "label3";
-            label3.Size = new Size(208, 19);
-            label3.TabIndex = 5;
-            label3.Text = "South Dakota State University";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(102, 165);
-            label4.Margin = new Padding(2, 0, 2, 0);
-            label4.Name = "label4";
-            label4.Size = new Size(354, 19);
-            label4.TabIndex = 6;
-            label4.Text = "Department Of Civil and Environmental Engineering";
+
             // 
             // label5
             // 
@@ -132,37 +84,13 @@
             label6.Text = resources.GetString("label6.Text");
             label6.Click += label6_Click;
             // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label7.Location = new Point(147, 197);
-            label7.Name = "label7";
-            label7.Size = new Size(242, 15);
-            label7.TabIndex = 9;
-            label7.Text = "Principal Investigator : Dr. Akram Jwadhari";
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label8.Location = new Point(147, 223);
-            label8.Name = "label8";
-            label8.Size = new Size(201, 15);
-            label8.TabIndex = 10;
-            label8.Text = "Research Student : Ahmed Hussien";
-            // 
             // About
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(527, 352);
-            Controls.Add(label8);
-            Controls.Add(label7);
             Controls.Add(label6);
             Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(pictureBox2);
@@ -185,11 +113,7 @@
         private PictureBox pictureBox2;
         private Label label1;
         private Label label2;
-        private Label label3;
-        private Label label4;
         private Label label5;
         private Label label6;
-        private Label label7;
-        private Label label8;
     }
 }
